1006bc53a22e2867c2470d438ca18ea0f0715f3c
Laurent Cheylus <foxy@free.fr>
2024-01-08 10:10:07 +0100

    build job: run job with an external Bash script
    - In 'build' job, run bash script ci/openbsd_build.sh
    - Move bash commands from 'build' job in ci/openbsd_build.sh
    
    Implementation inspired by rustup project (GitHub workflow CI)
    https://github.com/rust-lang/rustup/blob/master/.github/workflows/ci.yaml
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

---
+++ ci/openbsd_build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Bash script to run build on GitHub CI
+#
+
+set -e
+
+REPO_NAME=${GITHUB_WORKSPACE##*/}
+WORKSPACE_PARENT="/home/runner/work/${REPO_NAME}"
+WORKSPACE="${WORKSPACE_PARENT}/${REPO_NAME}"
+
+echo "## whoami"
+whoami
+
+# Increase the number of file descriptors - See https://github.com/rust-lang/cargo/issues/11435
+ulimit -n 1024
+
+# Check environment
+echo "## environment"
+echo "CI='${CI}'"
+echo "REPO_NAME='${REPO_NAME}'"
+echo "WORKSPACE_PARENT='${WORKSPACE_PARENT}'"
+echo "WORKSPACE='${WORKSPACE}'"
+echo "NEXTEST_VERSION='${{ env.NEXTEST_VERSION }}'"
+env | sort
+
+# Tooling info
+echo "## tooling infos"
+cargo -V
+rustc -V
+
+## Run build script for cargo-nextest
+echo "##################################"
+echo "Run build script for cargo-nextest"
+echo "##################################"
+
+cd "${WORKSPACE}"
+export CARGO_TERM_COLOR=always
+./build.sh "${{ env.NEXTEST_VERSION }}"
+
+echo "# ls -l /tmp/cargo-nextest-build-${{ env.NEXTEST_VERSION }}/target/release"
+ls -l /tmp/cargo-nextest-build-${{ env.NEXTEST_VERSION }}/target/release
+
+echo "# /tmp/cargo-nextest-build-${{ env.NEXTEST_VERSION }}/target/release/cargo-nextest -V"
+/tmp/cargo-nextest-build-${{ env.NEXTEST_VERSION }}/target/release/cargo-nextest -V
+
+## Copy cargo-nextest binary to GITHUB_WORKSPACE => copy back to Ubuntu host
+echo "# Copy cargo-nextest-${{ env.NEXTEST_VERSION }} to working directory"
+cp -v /tmp/cargo-nextest-build-${{ env.NEXTEST_VERSION }}/target/release/cargo-nextest ${GITHUB_WORKSPACE}
