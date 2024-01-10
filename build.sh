dcb9ae8faa33c4c4f92bfd1860eeb5c4c1491691
Laurent Cheylus <foxy@free.fr>
2024-01-10 17:22:13 +0100

    build.sh: add optional arg for WRKDIR
    - Get WRKDIR from arg2, default value = /tmp/cargo-nextest-build-<VERSION>
    - Fix code with shellcheck
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- build.sh
+++ build.sh
@@ -9,10 +9,20 @@ set -eu
 # PROFILE="debug"
 PROFILE="release"
 
+usage() {
+	echo "usage: build.sh NEXTEST_VERSION [WRKDIR]"
+	printf "\n  - NEXTEST_VERSION\tversion of cargo-nextest to build\n"
+	printf "  - WRKDIR\t\tdirectory to download sources and build tool "
+	echo "(default = /tmp/cargo-nextest-build-<NEXTEST_VERSION>)"
+}
+
 # Check args to get version
-if [ $# -ne 1 ]; then
-	echo "ERROR: unable to get cargo-nextest version"
-	echo "usage: build.sh <NEXTEST_VERSION>"
+if [ $# -lt 1 ]; then
+	printf "ERROR: unable to get cargo-nextest version\n\n"
+	usage
+	exit 1
+elif [ $# -gt 2 ]; then
+	usage
 	exit 1
 fi
 
@@ -24,58 +34,57 @@ echo "[*] Build cargo-nextest version ${VERSION} on OpenBSD $(uname -r)"
 rm -f /tmp/cargo-nextest-"${VERSION}".tar.gz
 
 # Check version and download sources for cargo-nextest
-STATUSCODE=$(curl -sL "https://github.com/nextest-rs/nextest/archive/refs/tags/cargo-nextest-"${VERSION}".tar.gz" -O --output-dir /tmp --write-out "%{http_code}")
-if test $STATUSCODE -eq 404; then
-	rm -f /tmp/cargo-nextest-"${VERSION}".tar.gz
+STATUSCODE=$(curl -sL "https://github.com/nextest-rs/nextest/archive/refs/tags/cargo-nextest-${VERSION}.tar.gz" -O --output-dir /tmp --write-out "%{http_code}")
+if test "$STATUSCODE" -eq 404; then
 	echo "ERROR: non existent cargo-nextest version '${VERSION}'"
 	exit 1
 fi
 
-if test $STATUSCODE -gt 400; then
+if test "$STATUSCODE" -gt 400; then
 	echo "ERROR: unable to download sources for cargo-nextest - HTTP Status-Code = ${STATUSCODE}"
 	exit 1
 fi
 
-WRKDIR=/tmp/cargo-nextest-build-${VERSION}
+# Set WRKDIR from args (optional)
+WRKDIR=${2:-/tmp/cargo-nextest-build-"${VERSION}"}
 
 echo "[*] WRKDIR=${WRKDIR}"
-rm -rf ${WRKDIR}
 
 echo "[*] Download sources for cargo-nextest-build-${VERSION}"
 
 # Prepare sources for cargo-nextest
 tar xzf /tmp/cargo-nextest-"${VERSION}".tar.gz -C /tmp
-mv /tmp/nextest-cargo-nextest-"${VERSION}" ${WRKDIR}
+mv /tmp/nextest-cargo-nextest-"${VERSION}" "${WRKDIR}"
 rm -f /tmp/cargo-nextest-"${VERSION}".tar.gz
 
-mkdir -p ${WRKDIR}/crates
+mkdir -p "${WRKDIR}"/crates
 
 # Download crate openssl-sys
 echo "[*] Download sources for openssl-sys-0.9.97 crate"
-cd ${WRKDIR}
+cd "${WRKDIR}"
 curl -sL https://crates.io/api/v1/crates/openssl-sys/0.9.97/download|tar xzf - -C crates
 
 # Patch crate openssl-sys
 echo "[*] Patch sources for openssl-sys-0.9.97 crate"
-cd ${WRKDIR}/crates/openssl-sys-0.9.97
+cd "${WRKDIR}"/crates/openssl-sys-0.9.97
 sed -i.orig -e "/ => ('.', '.'),/h" -e "/ => ('.', '.', '.'),/h" -e "/_ => version_error(),/{g; s/(.*) =>/_ =>/; }" build/main.rs
 
 # Download crate zstd-sys-2.0.9+zstd.1.5.5
 echo "[*] Download sources for zstd-sys-2.0.9+zstd.1.5.5 crate"
-cd ${WRKDIR}
+cd "${WRKDIR}"
 curl -sL https://crates.io/api/v1/crates/zstd-sys/2.0.9+zstd.1.5.5/download|tar xzf - -C crates
 
 # Patch crate zstd-sys-2.0.9+zstd.1.5.5
 echo "[*] Patch sources for zstd-sys-2.0.9+zstd.1.5.5 crate"
-cd ${WRKDIR}/crates/zstd-sys-2.0.9+zstd.1.5.5
+cd "${WRKDIR}"/crates/zstd-sys-2.0.9+zstd.1.5.5
 rm -rf zstd
 sed -i.orig -e 's,^fn main() {,fn main() { println!("cargo:rustc-link-lib=zstd"); return;,' build.rs
-sed -i '1s/^/#![allow(unreachable_code)]\'$'\n/' build.rs
+sed -i "1s/^/#![allow(unreachable_code)]\'$'\n/" build.rs
 
 # Patch cargo configuration for modified crates
 echo "[*] Patch cargo configuration in .cargo/config.toml"
 
-cd ${WRKDIR}
+cd "${WRKDIR}"
 printf "\n[patch.crates-io]\n" > cargo_config-patch.toml
 printf "openssl-sys = { path = 'crates/openssl-sys-0.9.97'}\n" >> cargo_config-patch.toml
 printf "zstd-sys = { path = 'crates/zstd-sys-2.0.9+zstd.1.5.5' }\n" >> cargo_config-patch.toml
@@ -84,14 +93,14 @@ cat cargo_config-patch.toml >> .cargo/config.toml
 rm -f cargo_config-patch.toml
 
 echo "[*] Patch Cargo.toml to strip binary in release profile"
-cd ${WRKDIR}
+cd "${WRKDIR}"
 printf "\n[profile.release]\n" > Cargo-patch.toml
 printf "strip = true  # Automatically strip symbols from the binary.\n" >> Cargo-patch.toml
 
 cat Cargo-patch.toml >> Cargo.toml
 rm -f Cargo-patch.toml
 
-cd ${WRKDIR}
+cd "${WRKDIR}"
 # Build cargo-nextest with cargo - self-update feature disabled
 echo "[*] Build cargo-nextest with profile ${PROFILE}"
 if [ "${PROFILE}" = "release" ]; then
